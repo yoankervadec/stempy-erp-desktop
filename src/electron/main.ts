b9544fae02a6eb86c0cf5ad62b19132ee06e49f3
Yoan Kervadec <kervadecy@icloud.com>
2026-05-18 14:35:29 -0400

    global setup

---
+++ src/electron/main.ts
@@ -0,0 +1,17 @@
+import { app, BrowserWindow } from "electron";
+import path from "path";
+import { isDev } from "./util.js";
+
+type test = string;
+
+app.on("ready", () => {
+  const mainWindow = new BrowserWindow({});
+
+  if (isDev()) {
+    mainWindow.loadURL("http://localhost:5123");
+  } else {
+    mainWindow.loadFile(
+      path.join(app.getAppPath(), "dist-react", "index.html"),
+    );
+  }
+});
